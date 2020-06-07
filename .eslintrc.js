14410bb151a8dec88e91e46656983e9ca43b811f
zhangwencai <zhangwencai@qq.com>
2020-06-07 11:12:41 +0800

    第二天，用户登录与数据展现

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,9 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'space-before-funtion-paren': 0
+    'space-before-function-paren': 0,
+    'space-before-function-paren': 0,
+    'space-before-function-paren': 0,
+    'indent': 0
   }
 }
